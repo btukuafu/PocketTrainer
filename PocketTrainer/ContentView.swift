62d23a8ef977bfa1abb55b23ff27389df3ab1fb1
Bruce <stukuafu@sandiego.edu>
2023-02-09 12:36:18 -0800

    Added to the firebase authentication

--- PocketTrainer/ContentView.swift
+++ PocketTrainer/ContentView.swift
@@ -6,9 +6,43 @@
 //
 
 import SwiftUI
+import FirebaseAuth
+
+class AppViewModel: ObservableObject {
+    
+    let auth = Auth.auth()
+    
+    var isSignedIn: Bool {
+        return auth.currentUser != nil
+    }
+    
+    func signIn(email: String, password: String) {
+        auth.signIn(withEmail: email,
+                    password: password) { result, error in
+            guard result != nil, error == nil else{
+                        return
+                    }
+                    // success
+        }
+    }
+    
+    func signUp(email: String, password: String) {
+        auth.createUser(withEmail: email, password: password) { result, error in
+            guard result != nil, error == nil else{
+                return
+            }
+            // success
+        }
+    }
+    
+}
 
 struct ContentView: View {
     @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
     var body: some View {
         NavigationView {
             VStack {
@@ -20,10 +54,17 @@ struct ContentView: View {
                     TextField("Email Address", text: $email)
                         .padding()
                         .background(Color(.secondarySystemBackground))
-                    SecureField("Password", text: $email)
+                    SecureField("Password", text: $password)
                         .padding()
                         .background(Color(.secondarySystemBackground))
-                    Button(action:{}, label: {
+                    Button(action:{
+                        guard !email.isEmpty, !password.isEmpty else {
+                            return
+                        }
+                        
+                        viewModel.signIn(email: email, password: password)
+                    }, label: {
+                        
                         Text("Sign In")
                             .foregroundColor(Color.white)
                             .frame(width: 200, height: 50)
--- PocketTrainer/PocketTrainerApp.swift
+++ PocketTrainer/PocketTrainerApp.swift
@@ -10,10 +10,21 @@ import Firebase
 
 @main
 struct PocketTrainerApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+                ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application( _ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
     }
 }
 
