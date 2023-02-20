13a4e75afeb9e282acb44e471cceb2ae53b4f915
Bruce <stukuafu@sandiego.edu>
2023-02-20 01:20:04 -0800

    Created a separate view for the login page called sign up

--- PocketTrainer/ContentView.swift
+++ PocketTrainer/ContentView.swift
@@ -20,20 +20,30 @@ class AppViewModel: ObservableObject {
     
     func signIn(email: String, password: String) {
         auth.signIn(withEmail: email,
-                    password: password) { result, error in
+                    password: password) { [weak self] result, error in
             guard result != nil, error == nil else{
-                        return
-                    }
-                    // success
+                return
+            }
+            
+            DispatchQueue.main.async {
+                // success
+                self?.signedIn = true
+            }
+            
         }
     }
     
     func signUp(email: String, password: String) {
-        auth.createUser(withEmail: email, password: password) { result, error in
+        auth.createUser(withEmail: email, password: password) { [weak self]result, error in
             guard result != nil, error == nil else{
                 return
             }
-            // success
+            
+            DispatchQueue.main.async {
+                // success
+                self?.signedIn = true
+            }
+            
         }
     }
     
@@ -45,7 +55,7 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            if viewModel.isSignedIn{
+            if viewModel.signedIn{
                 Text("You are signed In")
             }
             else{
@@ -57,101 +67,140 @@ struct ContentView: View {
         }
     }
 }
-    
+
 struct SignInView: View {
-        @State var email = ""
-        @State var password = ""
-        
-        @EnvironmentObject var viewModel: AppViewModel
-        
-        var body: some View {
-            VStack {
-                Text("PocketTrainer")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-                    .padding()
-                
-                VStack{
-                    TextField("Email Address", text: $email)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                    SecureField("Password", text: $password)
-                        .padding()
-                        .background(Color(.secondarySystemBackground))
-                    
-                    Button(action:{
-                        guard !email.isEmpty, !password.isEmpty else {
-                            return
-                        }
-                        
-                        viewModel.signIn(email: email, password: password)
-                    }, label: {
-                        
-                        Text("Sign In")
-                            .foregroundColor(Color.white)
-                            .frame(width: 200, height: 50)
-                            .cornerRadius(8)
-                            .background(Color.blue)
-                        
-                    })
-                    
-                    HStack {
-                        Text("Or sign up with:")
-                            .font(.caption)
-                        
-                        Button(action: {
-                            // handle Twitter sign up
-                        }, label: {
-                            Image("Twitter")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30, height: 30)
-                                .padding()
-                        })
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                        
-                        Button(action: {
-                            // handle Facebook sign up
-                        }, label: {
-                            Image("Facebook")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 30, height: 30)
-                                .padding()
-                        })
-                        .background(Color(.systemGray6))
-                        .cornerRadius(8)
-                    }
-                    .padding(.top, 16)
-                    
-                    Button(action: {
-                        // handle forgot password
-                    }, label: {
-                        Text("Forgot your password?")
-                            .font(.caption)
-                            .foregroundColor(.blue)
-                    })
-                    .padding(.top, 8)
-                }
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+            Text("PocketTrainer")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
                 .padding()
+            
+            VStack{
+                TextField("Email Address", text: $email)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
                 
-                Spacer()
-                
-                Button(action: {
-                    // handle sign up
+                Button(action:{
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signIn(email: email, password: password)
                 }, label: {
+                    
+                    Text("Sign In")
+                        .foregroundColor(Color.white)
+                        .frame(width: 200, height: 50)
+                        .cornerRadius(8)
+                        .background(Color.blue)
+                    
+                })
+                
+                Text("Don't have an account?")
+                NavigationLink("Sign Up", destination: SignUpView())
+            }
+            .padding()
+            
+            Spacer()
+        }
+        .navigationTitle("Sign In")
+    }
+}
+
+struct SignUpView: View {
+    @State var email = ""
+    @State var password = ""
+    
+    @EnvironmentObject var viewModel: AppViewModel
+    
+    var body: some View {
+        VStack {
+            Text("PocketTrainer")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+                .padding()
+            
+            VStack{
+                TextField("Email Address", text: $email)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                SecureField("Password", text: $password)
+                    .padding()
+                    .background(Color(.secondarySystemBackground))
+                
+                Button(action:{
+                    guard !email.isEmpty, !password.isEmpty else {
+                        return
+                    }
+                    
+                    viewModel.signIn(email: email, password: password)
+                }, label: {
+                    
                     Text("Sign Up")
                         .foregroundColor(Color.white)
                         .frame(width: 200, height: 50)
                         .cornerRadius(8)
-                        .background(Color.green)
+                        .background(Color.blue)
+                    
                 })
-                .padding(.bottom, 16)
+                
+                HStack {
+                    Text("Or sign up with:")
+                        .font(.caption)
+                    
+                    Button(action: {
+                        // handle Twitter sign up
+                    }, label: {
+                        Image("Twitter")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .padding()
+                    })
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    
+                    Button(action: {
+                        // handle Facebook sign up
+                    }, label: {
+                        Image("Facebook")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 30, height: 30)
+                            .padding()
+                    })
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                }
+                .padding(.top, 16)
+                
+                Button(action: {
+                    // handle forgot password
+                }, label: {
+                    Text("Forgot your password?")
+                        .font(.caption)
+                        .foregroundColor(.blue)
+                })
+                .padding(.top, 8)
             }
-            .navigationTitle("Sign In")
+            .padding()
+            
+            Spacer()
+            
         }
+        .navigationTitle("Sign Up")
+    }
 }
 
 
