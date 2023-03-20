44f0ff3d301302069a31da37dadda396edecce7a
Bruce <stukuafu@sandiego.edu>
2023-03-20 15:14:53 -0700

    Bluetooth update

---
+++ PocketTrainer/BluetoothViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  BluetoothViewModel.swift
+//  PocketTrainer
+//
+//  Created by Bruce Tukuafu on 3/19/23.
+//
+import UIKit
+import CoreBluetooth
+
+class BluetoothViewModel: NSObject, ObservableObject{
+    private var centralManager: CBCentralManager?
+    private var peripherals: [CBPeripheral] = []
+    @Published var peripheralNames: [String] = []
+    var device = ""
+    override init() {
+        super.init()
+        self.centralManager = CBCentralManager(delegate: self, queue: .main)
+        
+    }
+}
+
+extension BluetoothViewModel: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            self.centralManager?.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !peripherals.contains(peripheral) {
+            self.peripherals.append(peripheral)
+            if peripheral.name != nil {
+                self.peripheralNames.append(peripheral.name ?? "nil")
+            }
+        }
+    }
+}
